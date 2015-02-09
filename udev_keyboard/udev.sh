95e35942eff20f53ea75bc0eb4e1d1bc16a40a65
俊杰Jerry <bupt1987@gmail.com>
2015-02-09 19:11:41 +0800

    up

--- udev_keyboard/udev.sh
+++ udev_keyboard/udev.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo -u "${USER}" /usr/bin/xmodmap -display :0 /home/junjie/conf/.Xmodmap >/dev/null 2>&1 &
+sudo -u "${USER}" /usr/bin/xmodmap -display :0 ~/conf/.Xmodmap >/dev/null 2>&1 &
