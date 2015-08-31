06e1103897ba7a03b98f57d0bdcfccfedf7c84e5
俊杰Jerry <bupt1987@gmail.com>
2015-08-31 11:19:00 +0800

    add mac

---
+++ script/mac_fpm.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+ 
+ 
+param=$1
+ 
+start()
+{
+    fpms=`ps aux | grep -i "php-fpm" | grep -v grep | awk '{print $2}'`
+    if [ ! -n "$fpms" ]; then
+        /usr/local/opt/php55/sbin/php-fpm --fpm-config /usr/local/etc/php/5.5/php-fpm.conf &
+	echo "PHP-FPM Start"
+    else
+        echo "PHP-FPM Already Start"
+    fi
+}
+ 
+stop()
+{
+    fpms=`ps aux | grep -i "php-fpm" | grep -v grep | awk '{print $2}'`
+    echo $fpms | xargs kill -9
+ 
+    for pid in $fpms; do
+        if echo $pid | egrep -q '^[0-9]+$'; then
+            echo "PHP-FPM Pid $pid Kill"
+        else
+            echo "$pid IS Not A PHP-FPM Pid"
+        fi
+    done
+}
+ 
+case $param in
+    'start')
+        start;;
+    'stop')
+        stop;;
+    'restart')
+        stop
+        start;;
+    *)
+        echo "Usage: phpfpm start|stop|restart";;
+esac
