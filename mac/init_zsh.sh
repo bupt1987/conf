34ff636da40c1d1569d96b0239f1bdd4d4dd4511
俊杰Jerry <bupt1987@gmail.com>
2015-11-06 18:58:34 +0800

    up

--- mac/init_zsh.sh
+++ mac/init_zsh.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+dir=$(cd `dirname $0`; pwd);
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" \
- && git clone git@gist.github.com:18fd7c24360c68bab884.git /tmp/xxf.zsh-theme \
- && mv /tmp/xxf.zsh-theme/xxf.zsh-theme ~/.oh-my-zsh/themes/ \
- && echo "ZSH_THEME=xxf" >> ~/.zshrc \
- && echo 'success'
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+rm -fr /tmp/xxf.zsh-theme
+git clone git@gist.github.com:18fd7c24360c68bab884.git /tmp/xxf.zsh-theme
+rm -f ~/.oh-my-zsh/themes/xxf.zsh-theme
+cp /tmp/xxf.zsh-theme/xxf.zsh-theme ~/.oh-my-zsh/themes/ 
+
+if [ -f ~/.zshrc-backup ]; then
+	rm -f ~/.zshrc-backup
+fi
+cat ~/.zshrc | sed 's/ZSH_THEME=".*/ZSH_THEME="xxf"/g' > ~/zshrc
+mv ~/.zshrc ~/.zshrc-backup
+mv ~/zshrc ~/.zshrc
+
+echo 'success'
 
