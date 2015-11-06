24f9b663c826cf7dad7ad9e58dec36dd45f2ea78
俊杰Jerry <bupt1987@gmail.com>
2015-11-06 19:01:53 +0800

    up

--- mac/init_zsh.sh
+++ mac/init_zsh.sh
@@ -3,10 +3,17 @@ dir=$(cd `dirname $0`; pwd);
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-rm -fr /tmp/xxf.zsh-theme
-git clone git@gist.github.com:18fd7c24360c68bab884.git /tmp/xxf.zsh-theme
-rm -f ~/.oh-my-zsh/themes/xxf.zsh-theme
-cp /tmp/xxf.zsh-theme/xxf.zsh-theme ~/.oh-my-zsh/themes/ 
+if [ ! -d ~/.oh-my-zsh/themes/ ]; then
+	echo 'install oh-my-zs first'
+	exit
+fi
+
+if [ ! -f ]; then
+	rm -fr /tmp/xxf.zsh-theme
+	git clone git@gist.github.com:18fd7c24360c68bab884.git /tmp/xxf.zsh-theme
+	rm -f ~/.oh-my-zsh/themes/xxf.zsh-theme
+	cp /tmp/xxf.zsh-theme/xxf.zsh-theme ~/.oh-my-zsh/themes/
+fi
 
 if [ -f ~/.zshrc-backup ]; then
 	rm -f ~/.zshrc-backup
