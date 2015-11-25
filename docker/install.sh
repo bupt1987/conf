a9296bcc0a8387c4286e9524b9c8f7693bb55282
俊杰Jerry <bupt1987@gmail.com>
2015-11-25 11:36:38 -0800

    up

--- docker/install.sh
+++ docker/install.sh
@@ -5,7 +5,7 @@ PHP_DOCKER_NAME='php'
 
 cd $(cd `dirname $0`; pwd)
 
-sudo docker ps -a | grep -q ${PHP_DOCKER_NAME} && ( sudo docker stop ${PHP_DOCKER_NAME} ; sudo docker rm ${PHP_DOCKER_NAME} )
+docker ps -a | grep -q ${PHP_DOCKER_NAME} && ( sudo docker stop ${PHP_DOCKER_NAME} ; sudo docker rm ${PHP_DOCKER_NAME} )
 
 build_param=''
 if [[ "${@: -1}" == "reset" ]]; then
@@ -14,4 +14,4 @@ fi
 
 set -e
 
-sudo docker build ${build_param} --tag ${IMAGE_NAME} .
+docker build ${build_param} --tag ${IMAGE_NAME} .
