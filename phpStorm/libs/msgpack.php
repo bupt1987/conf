edabcac9b9db308dc6f78a0c4195100c75b5381f
俊杰Jerry <bupt1987@gmail.com>
2016-05-25 10:18:56 +0800

    up

---
+++ phpStorm/libs/msgpack.php
@@ -0,0 +1,28 @@
+<?php
+// Start of msgpack v.
+
+/**
+ * (PHP 5 &gt;= 5.2.0, PECL msgpack &gt;= 1.2.0)<br/>
+ * Returns the JSON representation of a value
+ * @link https://github.com/msgpack/msgpack-php
+ * @param mixed $value <p>
+ * The <i>value</i> being encoded. Can be any type except
+ * a resource.
+ * </p>
+ * @return string a encoded string on success or <b>FALSE</b> on failure.
+ */
+function msgpack_pack ($value) {}
+
+/**
+ * (PHP 5 &gt;= 5.2.0, PECL msgpack &gt;= 1.2.0)<br/>
+ * Decodes a JSON string
+ * @link https://github.com/msgpack/msgpack-php
+ * @param string $str <p>
+ * The <i>str</i> string being decoded.
+ * </p>
+ * @return mixed the value encoded
+ */
+function msgpack_unpack ($str) {}
+
+// End of msgpack v.
+?>
