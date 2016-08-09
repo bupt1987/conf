dc360800fa527d98f95b8b1958d54cdf7f6f1e2d
俊杰Jerry <bupt1987@gmail.com>
2016-08-09 16:18:41 +0800

    up

--- script/tmux_dw_status.sh
+++ script/tmux_dw_status.sh
@@ -10,10 +10,10 @@ tmux_init(){
 	#${tmux} select-pane -t 0
 #	${tmux} split-window -h
 
-	${tmux} select-pane -t 0
+	${tmux} select-pane -t 1
 	${tmux} send-keys "dw_terror" C-m
 
-	${tmux} select-pane -t 1
+	${tmux} select-pane -t 0
 	${tmux} send-keys "ssh dw-user-log '/www/calc_ram.php && tail -F /www/logs/run/scan_user_log.log'" C-m
 
 	#${tmux} select-pane -t 2
