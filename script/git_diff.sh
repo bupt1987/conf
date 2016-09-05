1d663bb59a1e5dd5d02d2924267d021c0c3a1d5a
俊杰Jerry <bupt1987@gmail.com>
2016-09-05 11:13:18 +0800

    up

--- script/git_diff.sh
+++ script/git_diff.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #alias gd='git diff --no-ext-diff -b -w -z --color=never | ~/conf/script/git_diff.php | less'
-
 export LINES
-
+if [ -z $LINES ]; then
+    LINES=25
+fi
 let height=$LINES-3
 
 count=`git diff --no-ext-diff -b -w -z --color=never $@ | ~/conf/script/git_diff.php | head -n $height | wc -l`
